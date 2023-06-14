8a97b770db4caf32c6babadbc6753533a9b4dcfd
niju.prem <nijuprem7@gmail.com>
2023-06-14 21:26:46 +0530

    Built Responsive layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,29 @@
-import { Button, ButtonGroup } from "@chakra-ui/react";
+import { Button, ButtonGroup, Grid, GridItem, Show } from "@chakra-ui/react";
 
 function App() {
   return (
     <>
-      <Button colorScheme="blue">Button</Button>
+      <Grid
+        templateAreas={{
+          base: `"nav" "main"`,
+          lg: `"nav nav" "aside main"`,
+        }}
+      >
+        <GridItem area={"nav"} bg="orange.300">
+          {" "}
+          Nav{" "}
+        </GridItem>
+        <Show above="lg">
+          <GridItem area={"aside"} bg="red.300">
+            {" "}
+            Aside{" "}
+          </GridItem>
+        </Show>
+        <GridItem area={"main"} bg="blue.300">
+          {" "}
+          Main{" "}
+        </GridItem>
+      </Grid>
     </>
   );
 }
