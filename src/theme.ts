e0155e2e26ca04dd59477b4a19af060fd25d390b
niju.prem <nijuprem7@gmail.com>
2023-06-15 14:44:31 +0530

    Built the color mode switch

--- src/theme.ts
+++ src/theme.ts
@@ -5,12 +5,11 @@ const config = {
   useSystemColorMode: false,
 }
 
-// 3. extend the theme
 const theme = extendTheme({ config })
 
 // const theme = extendTheme({ 
 //     config : {
-//     initialColorMode: 'light',
+//     initialColorMode: 'dark',
 //     useSystemColorMode: false,
 //   } })
 
