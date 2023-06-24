b329276bb7c65d4c0aa881e387c50553a9882bc6
niju.prem <nijuprem7@gmail.com>
2023-06-24 16:08:53 +0530

    Built Search Input

--- src/services/image-url.ts
+++ src/services/image-url.ts
@@ -1,7 +1,7 @@
 
 const getCroppedImageUrl =(url: string)=>{
-    if(!url) return ''
-    
+    if(!url) return 'https://picsum.photos/600/400'
+
     const target= 'media/';
     const index= url.indexOf(target) + target.length ;
     return url.slice(0, index) + 'crop/600/400/' + url.slice(index)
