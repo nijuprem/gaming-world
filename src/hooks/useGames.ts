f17cd4e4d91df8905a665abb628cf4f3fded38d4
niju.prem <nijuprem7@gmail.com>
2023-07-05 16:58:38 +0530

    Refactoring: Create a reusable API Client

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,7 +1,7 @@
 import { useQuery } from '@tanstack/react-query';
 import { GameQuery } from '../App';
 import { Platform } from './usePlatform';
-import apiClient, { FetchResponse } from '../services/api-client';
+import APIClient, { FetchResponse } from '../services/api-client';
 
 export interface Game {
     id: number;
@@ -11,14 +11,14 @@ export interface Game {
     metacritic: number
   }
 
+  const apiClient = new APIClient<Game>('/games');
 
 const useGames = (gameQuery: GameQuery) => 
 
 useQuery<FetchResponse<Game>, Error>({
   queryKey: ['games', gameQuery],
   queryFn: () => 
-  apiClient
-  .get<FetchResponse<Game>>('/games', {
+  apiClient.getAll({
     params:{ 
       genres: gameQuery.genre?.id, 
       parent_platforms:  gameQuery.platform?.id ,
@@ -26,7 +26,6 @@ useQuery<FetchResponse<Game>, Error>({
       search: gameQuery.searchText
     }
   })
-  .then(res=> res.data)
 })
 
 export default useGames
--- src/hooks/usePlatform.ts
+++ src/hooks/usePlatform.ts
@@ -1,6 +1,6 @@
 import { useQuery } from '@tanstack/react-query';
 import platforms from '../data/platforms'
-import apiClient, { FetchResponse } from '../services/api-client';
+import APIClient from '../services/api-client';
 
 export interface Platform{
     id: number,
@@ -8,12 +8,11 @@ export interface Platform{
     slug: string
 }
 
+const apiClient = new APIClient<Platform>('/platforms/lists/parents');
+
 const usePlatform = () => useQuery({
     queryKey: ['platforms'],
-    queryFn: ()=> 
-    apiClient
-    .get('/platforms/lists/parents')
-    .then<FetchResponse<Platform>>(res=> res.data),
+    queryFn: apiClient.getAll,
     staleTime: 24 * 60 * 60 * 1000,
     initialData: {count: platforms.length ,results: platforms}
 })
